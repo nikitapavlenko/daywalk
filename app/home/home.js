5335a0e028d8e3fdb5454002616695fd15de9ad8
Nikita Pavlenko <nikitapavlenko@ukr.net>
2015-04-25 21:24:28 +0300

    added home page

--- app/home/home.js
+++ app/home/home.js
@@ -4,11 +4,17 @@ angular.module('daywalk.home', ['ngRoute'])
 
 .config(function($routeProvider) {
 	$routeProvider.when('/', {
-		templateUrl: 'app/home/home.html',
-		controller: 'HomeController'
+		templateUrl: 'app/home/home.html'
 	});
+	$routeProvider.when('/cities', {
+		templateUrl: 'app/home/cities.html'
+	});
+	$routeProvider.when('/tours', {
+		templateUrl: 'app/home/tours.html'
+	});
+
+
 })
-
 .controller('HomeController', ['$scope', function($scope) {
-
 }]);
+
